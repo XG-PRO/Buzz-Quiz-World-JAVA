d4af0ba5f018fa36c3efba9c5bbaa5595f5017af
XG-PRO <knperros@csd.auth.gr>
2020-11-13 18:52:46 +0200

    Initial Commit

--- src/Question.java
+++ src/Question.java
@@ -1,57 +1,61 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Question {
     /**
-     * The question string
+     * The question itself string
      */
-    private String question;
+    private String name;
+
     /**
      * The type of the question is stored in a number
-     * 1 = Σωστή απάντηση,
-     * 2 = Σταμάτησε το χρονόμετρο
-     * 3 = Ποντάρισμα
-     * 4 = Γρήγορη απάντηση
-     * 5 = Θερμόμετρο
+     * 1 = Ιστορια
+     * 2 = Γεωγραφία
+     * 3 = Τέχνες
+     * 4 = Τεχνολογία
+     * 5 = Θρησκεία
      */
+
     private final int type;
     /**
      * The responses are stored in a list
      */
     private String[] responses_array;
     //private String path_to_image;
-
+    private boolean has_been_visited;
     /**
      * Default constructor
      * @param type The type of the question (1= Σωστή απάντηση,2 = Σταμάτησε το χρονόμετρο, 3 = Ποντάρισμα, 4 = Γρήγορη απάντηση, 5 = Θερμόμετρο)
      * @param responses_array A String array that contains the responses,
      *                      THE FIRST RESPONSE IS THE RIGHT ONE
      */
-    public Question(String question,int type, String[] responses_array) {
-        if (type<1 || type>5){
-            throw new IllegalArgumentException(
-                    "SFALMA! Argument 'type' should be in the interval 1 <= type< question <= 5");
-        }
-        this.question = question;
+    public Question(String name,int type, String[] responses_array) {
+        this.name = name;
         this.type = type;
-        this.responses_array = new String[4];
+        this.responses_array = new String[responses_array.length];
         System.arraycopy(responses_array,0,this.responses_array,0,responses_array.length);
-
+        has_been_visited = false;
     }
     /**
-     * @return An String array that contains the responses
+     * @return A String array that contains the responses
      */
-    public  String[] getResponses(){
+    public String[] getResponses(){
         String[] temp = new String[responses_array.length];
         System.arraycopy(responses_array,0,temp,0,responses_array.length);
         return temp;
     }
-
     public String getQuestion() {
-        return question;
+        return name;
     }
 
     public int getType() {
         return type;
     }
-
+    public boolean getVisited(){
+        return has_been_visited;
+    }
+    public void setToVisited(){
+        has_been_visited = true;
+    }
 }
--- src/Questions.java
+++ src/Questions.java
@@ -1,3 +1,40 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Random;
+
 public class Questions {
 
+    private ArrayList<Question> question_array;
+    private HashMap<Integer, ArrayList<Question>> hash;
+
+    public Questions() {
+        question_array = new ArrayList<>();
+    }
+
+    /**
+     *
+     * @param name The name of the question
+     * @param type The type of the question
+     * @param responses_array String array that contains the responses,
+     *                        THE FIRST RESPONSE IS THE RIGHT ONE
+     */
+
+    public void add_Question(String name, int type, String[] responses_array) {
+        //question_array.add(new Question(name,type,responses_array));
+        if (!hash.containsKey(type)){
+            hash.put(type,new ArrayList<>());
+        }
+        hash.get(type).add(new Question(name,type,responses_array));
+    }
+    public void get_random_question(int type){
+        if (type==0){
+            int random =getRandomNumberUsingNextInt(1,hash.keySet().size());
+        }
+
+    }
+    public static int getRandomNumberUsingNextInt(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min) + min;
+    }
 }
